3abcad4838d4c45d6c202010e7bb3c8149d77120
Rishad Yamnoor <EvolOfThings@users.noreply.github.com>
2018-02-19 18:26:24 +0530

    Character Component is created, no logic yet

---
+++ src/CharComponent.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const CharComponent = (props) => {
+  const listStyle = {
+    display: 'inline-block', 
+    padding: '16px', 
+    textAlign: 'center', 
+    margin: '16px', 
+    border: '1px solid black'
+  };
+  return (
+    <div style={listStyle}>
+    </div>
+  );
+}
+
+export default CharComponent;
