f6998125f5f27d7bf45d3781f07a9a172e39e8ea
Rishad Yamnoor <EvolOfThings@users.noreply.github.com>
2018-02-19 18:20:08 +0530

    Validating the text string length is working

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ class App extends Component {
         <h2>React-Practice-2</h2>
         <ol>
           <li>DONE Create an input field (in App component) with a change listener which outputs the length of the entered text below it (e.g. in a paragraph).</li>
-          <li>Create a new component (=> ValidationComponent) which receives the text length as a prop</li>
+          <li>DONE Create a new component (=> ValidationComponent) which receives the text length as a prop</li>
           <li>Inside the ValidationComponent, either output "Text too short" or "Text long enough" depending on the text length (e.g. take 5 as a minimum length)</li>
           <li>Create another component (=> CharComponent) and style it as an inline box (=> display: inline-block, padding: 16px, text-align: center, margin: 16px, border: 1px solid black).</li>
           <li>Render a list of CharComponents where each CharComponent receives a different letter of the entered text (in the initial input field) as a prop.</li>
@@ -31,7 +31,7 @@ class App extends Component {
         </ol>
         <p>Hint: Keep in mind that JavaScript strings are basically arrays!</p>
 
-        <input type="text" onChange={this.textLength} />
+        <input type="text" placeholder="Enter Text here" onChange={this.textLength} />
         <p>length = {this.state.length}</p>
         <ValidationComponent length={this.state.length} />
       </div>
--- src/ValidationComponent.js
+++ src/ValidationComponent.js
@@ -3,7 +3,16 @@ import React from 'react';
 const ValidationComponent = (props) => {
 return (
   <div>
-    <p> text length is {props.length} </p>
+    {
+      (props.length < 5)
+      ? <div> <p> Text is too short </p></div>
+      : null 
+    }
+    { 
+      (props.length > 10) 
+      ? <div> <p> Text is too long </p></div>
+      : null 
+    }
   </div>
 );
 }
@@ -23,3 +32,9 @@ export default ValidationComponent;
 //     <p>Text too long</p>
 //   </div
 //   }
+
+// if ({ props.length } <= 3) {
+//   <p>Text too short</p>
+// } else if ({ props.length } >= 8) {
+//   <p>Text too long</p>
+// }
